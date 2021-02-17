60d1d6ea8803cf5a100b43e92216e724496abc34
jose <jcmcampos@fc.ul.pt>
2021-02-17 11:23:18 +0000

    Added two other methods.

--- Foo.java
+++ Foo.java
@@ -5,4 +5,10 @@ public class Foo {
     public void bar(int x) {
         // NO-OP
     }
+    public void bar(java.util.List<?> l) {
+        // NO-OP
+    }
+    public void bar(java.util.ArrayList<Integer> a) {
+        // NO-OP
+    }
 }
